414fdaab0adb0a66df95f77878f0fa1667ba9b03
wow800915 <wow800915@gmail.com>
2024-09-01 23:47:42 +0800

    把url傳到newsFragment

--- app/src/main/java/com/weiyou/attractions/ui/home/HomeFragment.kt
+++ app/src/main/java/com/weiyou/attractions/ui/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.lifecycleScope
@@ -109,7 +108,9 @@ class HomeFragment : Fragment() {
 
         homeAdapter.setOnNewsItemClickListener(object : HomeAdapter.OnNewsItemClickListener {
             override fun onNewsItemClick(url: String) {
-                Toast.makeText(context, url, Toast.LENGTH_SHORT).show()
+                val bundle = Bundle()
+                bundle.putString("url", url)
+                findNavController().navigate(R.id.action_homeFragment_to_newsFragment, bundle)
             }
         })
     }
--- app/src/main/java/com/weiyou/attractions/ui/news/NewsFragment.kt
+++ app/src/main/java/com/weiyou/attractions/ui/news/NewsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.weiyou.attractions.databinding.FragmentNewsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,6 +22,12 @@ class NewsFragment : Fragment() {
         // Initialize View Binding
         _binding = FragmentNewsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val url = arguments?.getString("url")
+        Toast.makeText(context, url, Toast.LENGTH_LONG).show()
     }
 
     override fun onDestroyView() {
