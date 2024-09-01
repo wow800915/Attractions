eaf7f2b1cfcf0e7bf1a6d2f61d6830311da022a4
wow800915 <wow800915@gmail.com>
2024-09-01 16:49:17 +0800

    更改一下HomeRVItem的命名

--- app/src/main/java/com/weiyou/attractions/ui/home/HomeItem.kt
+++ app/src/main/java/com/weiyou/attractions/ui/home/HomeItem.kt
@@ -7,10 +7,22 @@ sealed class HomeItem {
     abstract val homeType: Int
 }
 
+data class HomeNewsTitleItem(
+    val title: String
+) : HomeItem() {
+    override val homeType: Int = 1//TODO 這邊的數字改一下 改成常數
+}
+
 data class HomeNewsItem(
     val newsItem: NewsItem
 ) : HomeItem() {
     override val homeType: Int = 2
+}
+
+data class HomeAttractionsItem(
+    val title: String
+) : HomeItem() {
+    override val homeType: Int = 3
 }
 
 data class HomeAttraction(
